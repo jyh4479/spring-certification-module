d9ccdc4aca1446909437eddda65cb6c454c37756
jyh4479 <jyh.dev@gmail.com>
2023-01-22 22:11:52 +0900

    [INIT]: Create project

---
+++ src/main/java/module/springcertification/SpringCertificationModuleApplication.java
@@ -0,0 +1,13 @@
+package module.springcertification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCertificationModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCertificationModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/module/springcertification/SpringCertificationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package module.springcertification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCertificationModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
